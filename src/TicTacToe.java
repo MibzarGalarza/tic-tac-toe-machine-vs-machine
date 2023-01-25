5c442e5803f53d5df89d83ec8ec594ff50061eb9
Mibzar <mibzar17@gmail.com>
2023-01-24 19:07:00 -0800

    comments

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,6 +4,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class TicTacToe {
 	static int[][] field = new int[3][3];
 	static Scanner scanner = new Scanner(System.in);
+
+	//I put this meessage for save time
 	static final String ERROR_MESSAGE = "Invalid input!";
 	static final String[] ROW_LABELS = { "A", "B", "C" };
 	static Node ai;
@@ -12,6 +14,7 @@ public class TicTacToe {
 	 * @param args
 	 */
 	public static void main(final String[] args) {
+		//The clear file is for the spaces free
 		clearField();
 
 		
@@ -21,27 +24,36 @@ public class TicTacToe {
 		for (;;) {
 			Node current = ai;
 			boolean player = Node.playsAsMin;
+			//The table is print in the terminal
 			printField();
 
 			for (;;) {
+				//So here is when the machine decide the move
 				System.out.println("Player " + (player ? "X" : "O") + " is about to set");
 
+				//The first player is the player X he feld depends the bucle and decide the number between 1-9
 				if (player) {
 					current = current.getChildWithValue();
 					field = current.copyField();
 					
-				}else{
-					final int[] indices = getInput();
+				}
+				//The second player is the player O he feld depends the random number beetween 1-9
+				else{
+					final int[] indices = getRandom();
 					field[indices[0]][indices[1]] = player ? 1 : 2;
 					current = current.children[indices[0]][indices[1]];
-					
 				}
-	
+
 				printField();
+
+				//He print the player winner and the system exit
 				if (hasWon()) {
 					System.out.println("Player " + (player ? "X" : "O") + " won!\n");
 					System.exit(0);
 				}
+
+				//If is a draw print the msg It's a draw. If the machine chooses a number already 
+				//chosen or filled in the table, it makes a break and chooses another number.
 
 				if (fieldFull()) {
 					System.out.println("It's a draw!\n");
@@ -55,6 +67,7 @@ public class TicTacToe {
 	}
 
 
+	//Here is when the table is print
 
 	static void printField(int[][] field) {
 		System.out.println("     1  2  3");
@@ -65,6 +78,7 @@ public class TicTacToe {
 			for (int j = 0; j < field[i].length; j++) {
 
 				System.out.print("|" + (field[i][j] == 0 ? "_" : field[i][j] == 1 ? "X" : "O") + "|");
+				//Here is when is decided who field is free space or is field.
 
 			}
 			System.out.print("   " + ROW_LABELS[i]);
@@ -81,11 +95,15 @@ public class TicTacToe {
 		return hasWon(field);
 	}
 
+
+	//Here is decided why you need for win the game.
 	public static boolean hasWon(int[][] field) {
 		for (int i = 0; i < field.length; i++) {
+
 			if (field[i][0] == field[i][1] && field[i][1] == field[i][2] && field[i][0] != 0) {
 				return true;
 			}
+			
 			if (field[0][i] == field[1][i] && field[1][i] == field[2][i] && field[0][i] != 0) {
 				return true;
 			}
@@ -115,7 +133,10 @@ public class TicTacToe {
 		return fieldFull(field);
 	}
 
-	private static int[] getInput() {
+	//Here is when the player O get the number random between 1-9 i use the method 
+	//tread class random number and convert to in a int and export the variable.
+
+	private static int[] getRandom() {
 		int value;
 		for (;;) {
 			try {
